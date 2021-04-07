2f13de8f030e0d6dee9556037e86b657cb92d79e
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-04-07 10:49:12 +0900

    Publishing messages

---
+++ PublishingMessagesToQueue/src/main/java/com/mezonworks/rabbitmq/Publisher.java
@@ -0,0 +1,22 @@
+package com.mezonworks.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Publisher {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        String message = "The first message from the RabbitMQ world.";
+
+        channel.basicPublish("", "Queue-1", null, message.getBytes());
+        channel.close();
+        connection.close();
+    }
+}
